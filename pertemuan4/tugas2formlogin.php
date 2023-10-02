f6a34c6054ec71691b9536bb141195de266065f2
AnissaNurrahmahh <anissaicunk@gmail.com>
2023-10-02 18:15:41 +0800

    menambahkan tugas praktikum 4

---
+++ pertemuan4/tugas2formlogin.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Form Login</title>
+</head>
+
+<body>
+
+<form id="form_login" name="form_login" method="POST" 
+action="proses_tugas2formlogin.php">
+    <p>Username <input type="text" name="username"
+    id="username" /></p> <p>Password <input 
+        type="text" name="password"
+        id="password"/></p> <p><input
+        type="submit" name="Button" 
+        id="Button" 
+value="masuk"/></p>   
+</form>
+
+</body>
+</html>
